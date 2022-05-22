d230b7a5e493c18bec8ab7cf9d4d185e5b2fa9fd
Munish Sharma <munish.usit@gmail.com>
2022-05-22 17:09:44 +0530

    Update custom.js
    fixed issues

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -46,8 +46,9 @@ $(document).ready(function(){
 			event.preventDefault();
 			var anchor = $(this);
 			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
+				scrollTop: $(anchor.attr('href')).offset().top - 90
 			}, 1200,'easeInOutExpo');
+			$(".navbar-toggle").click();
 		});
 		
 		$('body').scrollspy({
